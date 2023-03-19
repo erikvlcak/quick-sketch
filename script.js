08d5065c6e084fe464f3ee81e7e93d65e3a5b3dc
erikvlcak <erikvlcak1@gmail.com>
2023-03-19 02:03:56 +0100

    Finish code for showing/hiding sub-options.

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
-let arrow = document.querySelectorAll('.arrow');
-arrow.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        e.target.classList.toggle('bx-rotate-180');
-        e.target.nextElementSibling.classList.replace('invisible', 'visible');
-    })
+let optionsList = document.querySelectorAll('.optionsList li');
+let subOptionsList = document.querySelectorAll('.optionsList li .subOptions');
+let range = document.querySelector('#range');
+
+
+range.addEventListener('input', () => {
+    document.querySelector('.option_size-value').textContent = `${range.value} x ${range.value}`;
 })
+
+
+optionsList.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        if (e.target.parentElement.querySelector('.arrow').classList.contains('bx-rotate-180')) {
+            hideSubOptions(e);
+        } else {
+            showSubOptions(e);
+        }
+    })
+});
+
+document.addEventListener('mouseover', (e) => {
+    hideSubOptionsMouseOut(e);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showSubOptions(e) {
+    e.target.parentElement.querySelector('.arrow').classList.add('bx-rotate-180');
+    e.target.parentElement.querySelector('.subOptions').classList.replace('display-none', 'display-grid');
+    e.target.parentElement.querySelector('.subOptions').style.opacity = 1;
+    e.target.parentElement.querySelector('.subOptions').style.pointerEvents = 'initial';
+    e.target.parentElement.classList.toggle('visible');
+}
+
+function hideSubOptions(e) {
+    e.target.parentElement.querySelector('.arrow').classList.remove('bx-rotate-180');
+    e.target.parentElement.querySelector('.subOptions').classList.replace('display-grid', 'display-none');
+    e.target.parentElement.querySelector('.subOptions').style.opacity = 0;
+    e.target.parentElement.querySelector('.subOptions').style.pointerEvents = 'none';
+    e.target.parentElement.classList.toggle('visible');
+}
+
+function hideSubOptionsMouseOut(e) {
+    if ((e.target.tagName == 'BODY') || (e.target.className == 'optionsList')) {
+        document.querySelectorAll('.visible').forEach((item) => {
+            item.querySelector('.arrow').classList.remove('bx-rotate-180');
+            item.querySelector('.subOptions').classList.replace('display-grid', 'display-none');
+            item.querySelector('.subOptions').style.opacity = 0;
+            item.querySelector('.subOptions').style.pointerEvents = 'none';
+            item.classList.toggle('visible');
+        })
+    }
+}
