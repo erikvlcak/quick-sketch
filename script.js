d4fa1348e997aa77b5fcf107b7f6d2893ab0025c
erikvlcak <erikvlcak1@gmail.com>
2023-03-19 21:34:24 +0100

    Create script for canvas creation.

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 let optionsList = document.querySelectorAll('.optionsList li');
 let subOptionsList = document.querySelectorAll('.optionsList li .subOptions');
 let range = document.querySelector('#range');
+let canvas = document.querySelector('#canvas');
+let pixel = document.querySelectorAll
 
 
-range.addEventListener('input', () => {
-    document.querySelector('.option_size-value').textContent = `${range.value} x ${range.value}`;
+range.addEventListener('input', (e) => {
+    document.querySelector('.option_size-value').textContent = `${e.target.value} x ${e.target.value}`;
+    canvasSize = e.target.value;
+
+
 })
 
+range.addEventListener('click', (e) => {
+    console.log(e.target);
+    console.log(canvasSize);
+
+    while (canvas.hasChildNodes()) {
+        canvas.removeChild(canvas.lastChild);
+    };
+
+    canvas.style.gridTemplateColumns = 'repeat(' + canvasSize + ', minmax(auto, 1fr))';
+
+    for (let i = 0; i < (canvasSize * canvasSize); i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+
+
+
+        canvas.appendChild(pixel);
+    }
+})
 
 optionsList.forEach((item) => {
     item.addEventListener('click', (e) => {
-        if (e.target.parentElement.querySelector('.arrow').classList.contains('bx-rotate-180')) {
-            hideSubOptions(e);
-        } else {
-            showSubOptions(e);
+        if ((e.target.tagName == 'INPUT') || (e.target.tagName == 'LI')) {
+            if (e.target.parentElement.parentElement.querySelector('.arrow').classList.contains('bx-rotate-180')) {
+                hideSubOptionsClickListItem(e);
+            } else {
+                showSubOptionsClickListItem(e);
+            }
+        }
+
+        else if (e.target.tagName != 'INPUT') {
+            if (e.target.parentElement.querySelector('.arrow').classList.contains('bx-rotate-180')) {
+                hideSubOptionsClickList(e);
+            } else {
+                showSubOptionsClickList(e);
+            }
         }
     })
 });
@@ -24,7 +58,7 @@ document.addEventListener('mouseover', (e) => {
 
 document.querySelector('.bx-pencil').addEventListener('click', (e) => {
     e.target.classList.toggle('bx-spin');
-})
+});
 
 
 
@@ -39,7 +73,7 @@ document.querySelector('.bx-pencil').addEventListener('click', (e) => {
 
 
 
-function showSubOptions(e) {
+function showSubOptionsClickList(e) {
     e.target.parentElement.querySelector('.arrow').classList.add('bx-rotate-180');
     e.target.parentElement.querySelector('.subOptions').classList.replace('display-none', 'display-grid');
     e.target.parentElement.querySelector('.subOptions').style.opacity = 1;
@@ -47,7 +81,7 @@ function showSubOptions(e) {
     e.target.parentElement.classList.toggle('visible');
 }
 
-function hideSubOptions(e) {
+function hideSubOptionsClickList(e) {
     e.target.parentElement.querySelector('.arrow').classList.remove('bx-rotate-180');
     e.target.parentElement.querySelector('.subOptions').classList.replace('display-grid', 'display-none');
     e.target.parentElement.querySelector('.subOptions').style.opacity = 0;
@@ -55,8 +89,24 @@ function hideSubOptions(e) {
     e.target.parentElement.classList.toggle('visible');
 }
 
+function showSubOptionsClickListItem(e) {
+    e.target.parentElement.parentElement.querySelector('.arrow').classList.add('bx-rotate-180');
+    e.target.parentElement.parentElement.querySelector('.subOptions').classList.replace('display-none', 'display-grid');
+    e.target.parentElement.parentElement.querySelector('.subOptions').style.opacity = 1;
+    e.target.parentElement.parentElement.querySelector('.subOptions').style.pointerEvents = 'initial';
+    e.target.parentElement.parentElement.classList.toggle('visible');
+}
+
+function hideSubOptionsClickListItem(e) {
+    e.target.parentElement.parentElement.querySelector('.arrow').classList.remove('bx-rotate-180');
+    e.target.parentElement.parentElement.querySelector('.subOptions').classList.replace('display-grid', 'display-none');
+    e.target.parentElement.parentElement.querySelector('.subOptions').style.opacity = 0;
+    e.target.parentElement.parentElement.querySelector('.subOptions').style.pointerEvents = 'none';
+    e.target.parentElement.parentElement.classList.toggle('visible');
+}
+
 function hideSubOptionsMouseOut(e) {
-    if ((e.target.tagName == 'BODY') || (e.target.className == 'optionsList')) {
+    if ((e.target.id == 'content') || (e.target.id == 'canvas') || (e.target.className == 'optionsList')) {
         document.querySelectorAll('.visible').forEach((item) => {
             item.querySelector('.arrow').classList.remove('bx-rotate-180');
             item.querySelector('.subOptions').classList.replace('display-grid', 'display-none');
