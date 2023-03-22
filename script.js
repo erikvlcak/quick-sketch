18f4416bb7f8a51542c22e8a18db74ffcb03b898
erikvlcak <erikvlcak1@gmail.com>
2023-03-23 00:40:20 +0100

    Code all three coloring options.

--- script.js
+++ script.js
@@ -3,18 +3,77 @@ let range = document.querySelector('#range');
 let canvas = document.querySelector('#canvas');
 let pixel = document.querySelectorAll('.pixel');
 let pickColor = document.querySelector('.optionsList .option_color .subOptionPickColor');
+let colors = document.querySelector('.colors');
+let paintingColorList = document.querySelector('.option_color')
+let chosenColor = 'black';
+let rainbowColor = null;
+let canvasSize;
 
+//shows pre-defined selection of colors upon hovering over sub-option
 pickColor.addEventListener('mouseenter', (e) => {
     e.target.querySelector('.colors').classList.toggle('sub-displayed');
 })
-
 pickColor.addEventListener('mouseleave', (e) => {
     e.target.querySelector('.colors').classList.toggle('sub-displayed');
 })
 
 
 
+//allows you to pick color from pre-defined selection
 
+
+canvas.addEventListener('mousemove', (e) => {
+    if (mouseDrag) {
+        if (e.target.className == 'pixel') {
+            e.target.style.backgroundColor = chosenColor;
+        }
+    }
+});
+
+paintingColorList.addEventListener('click', (e) => {
+    let randomColor = null;
+    colors.querySelectorAll('div').forEach((item) => {
+        if (e.target.className == item.className) {
+            chosenColor = e.target.className;
+            canvas.style.borderColor = chosenColor;
+        }
+
+        if (e.target.className == 'subOptionRandomColor') {
+            randomColor = 'rgb(' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ')';
+            chosenColor = randomColor;
+            canvas.style.borderColor = randomColor;
+            console.log(`${randomColor}+'random color`);
+            console.log(`${chosenColor}+'random color`);
+
+        } else if (e.target.className == 'subOptionRainbow') {
+            canvas.addEventListener('mousemove', (e) => {
+                if (mouseDrag) {
+                    if (e.target.className == 'pixel') {
+                        rainbowColor = 'rgb(' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ')';
+                        e.target.style.backgroundColor = rainbowColor;
+                        canvas.style.borderColor = rainbowColor;
+                        console.log(randomColor);
+                        // uncomment for true epilepsy mode
+                        // document.querySelector('body').style.backgroundColor = randomColor;
+                    }
+                }
+            }
+            )
+
+        } else {
+            canvas.addEventListener('mousemove', (e) => {
+                if (mouseDrag) {
+                    if (e.target.className == 'pixel') {
+                        e.target.style.backgroundColor = chosenColor;
+                        randomColor = chosenColor;
+                        canvas.style.borderColor = chosenColor;
+                    }
+                }
+            }
+            )
+        }
+    })
+})
 
 
 //change canvas size according to slider and create canvasSize property
@@ -59,7 +118,7 @@ optionsList.forEach((item) => {
 
 //hide subOptions after mouse moves away
 document.addEventListener('mouseover', (e) => {
-    if ((e.target.id == 'content') || (e.target.id == 'canvas') || (e.target.className == 'pixel') || (e.target.className == 'optionsList')) {
+    if ((e.target.id == 'content') || (e.target.id == 'canvas') || (e.target.className == 'pixel') || (e.target.className == 'optionsList') || (e.target.className == 'canvasInitialMessage')) {
         document.querySelectorAll('.sub-displayed').forEach((item) => {
             item.classList.replace('sub-displayed', 'sub-hidden');
             item.parentElement.querySelector('.arrow').classList.remove('bx-rotate-180');
@@ -73,7 +132,6 @@ document.querySelector('.bx-pencil').addEventListener('click', (e) => {
 });
 
 //mouseover painting
-
 // canvas.addEventListener('mouseover', (e) => {
 //     if (e.target.className == 'pixel') {
 //         e.target.style.backgroundColor = 'black';
@@ -85,16 +143,9 @@ let mouseDrag = false;
 canvas.addEventListener('mousedown', () => {
     mouseDrag = true;
 })
-
 canvas.addEventListener('mouseup', () => {
     mouseDrag = false;
 })
 
-canvas.addEventListener('mousemove', (e) => {
-    if (mouseDrag) {
-        if (e.target.className == 'pixel') {
-            e.target.style.backgroundColor = 'black';
-        }
-    }
-})
+
 
