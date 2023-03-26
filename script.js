dd06be5b5c45006d171e664fe955b8ed3ca07f61
erikvlcak <erikvlcak1@gmail.com>
2023-03-26 01:03:25 +0100

    Complete shading and deleting functions.

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ let pixel = document.querySelectorAll('.pixel');
 let pickColor = document.querySelector('.optionsList .option_color .subOptionPickColor');
 let colors = document.querySelector('.colors');
 let paintingColorList = document.querySelector('.option_color')
-let chosenColor = 'black';
-let rainbowColor = null;
-let canvasSize;
+let shading = document.querySelector('.option_shading .subOptions');
+let deleting = document.querySelector('.option_delete .subOptions');
+let eraser = document.querySelector('.subOptionEraser');
+let chosenColor = 'hsl(0, 0%, 0%)';
+let colorOption = null;
+let shadingOption = null;
+let canvasSize, colorLightness, erasing;
+let chosenColorValue1, chosenColorValue2, chosenColorValue3;
 
-//shows pre-defined selection of colors upon hovering over sub-option
+//show color palette on hover
 pickColor.addEventListener('mouseenter', (e) => {
     e.target.querySelector('.colors').classList.toggle('sub-displayed');
 })
@@ -17,64 +22,119 @@ pickColor.addEventListener('mouseleave', (e) => {
     e.target.querySelector('.colors').classList.toggle('sub-displayed');
 })
 
+//coloring modes
+paintingColorList.addEventListener('click', (e) => {
+    shadingOption = null;
+    if (colorOption != 'rainbow') {
+        document.querySelectorAll('.option_shading').forEach((item) => {
+            item.style.opacity = '1';
+            item.style.pointerEvents = 'auto';
+        })
+    };
+    if (e.target.className == 'subOptionRandomColor') {
+        colorOption = 'random';
+        chosenColor = generateRandomColor();
+        canvas.style.borderImage = 'none';
+        canvas.style.borderColor = chosenColor;
+        console.log(chosenColor);
 
+    } else if (e.target.className == 'subOptionRainbow') {
+        colorOption = 'rainbow';
+        canvas.style.borderImage = 'repeating-linear-gradient(45deg,#c4e17f 0 50px,#f7fdca 0 100px,#fad071 0 150px,#f0766b 0 200px,#db9dbe 0 250px,#c49cdf 0 300px,#6599e2 0 350px,#61c2e4 0 400px) 10 ';
+        document.querySelectorAll('.option_shading').forEach((item) => {
+            item.style.opacity = '0.5';
+            item.style.pointerEvents = 'none';
+        })
+    } else {
+        colorOption = 'pick';
+        colors.querySelectorAll('div').forEach((item) => {
+            if (e.target.className == item.className) {
+                chosenColor = e.target.id;
+                canvas.style.borderImage = 'none';
+                canvas.style.borderColor = chosenColor;
+            }
+        })
+    }
+    colorLightness = getColorLightness(chosenColor);
 
-//allows you to pick color from pre-defined selection
+})
 
+//painting + shading + deleting
+let mouseDrag = false;
+canvas.addEventListener('mousedown', () => {
+    mouseDrag = true;
+})
+canvas.addEventListener('mouseup', () => {
+    mouseDrag = false;
+})
 
-canvas.addEventListener('mousemove', (e) => {
-    if (mouseDrag) {
-        if (e.target.className == 'pixel') {
+canvas.addEventListener('mouseover', (e) => {
+    if (erasing) {
+        if ((mouseDrag) && (e.target.className == 'pixel')) {
+            e.target.removeAttribute('style');
+        }
+    }
+    else if (colorOption === null || colorOption == 'pick' || colorOption == 'random') {
+        if ((mouseDrag) && (e.target.className == 'pixel')) {
+            if (shadingOption == 'darker') {
+                colorLightness -= 0.05;
+                chosenColor = 'hsl(' + chosenColorValue1 + ', ' + chosenColorValue2 + '%, ' + colorLightness + '%)';
+                console.log(chosenColor);
+            } else if (shadingOption == 'lighter') {
+                colorLightness += 0.05;
+                chosenColor = 'hsl(' + chosenColorValue1 + ', ' + chosenColorValue2 + '%, ' + colorLightness + '%)';
+                console.log(chosenColor);
+            }
+            e.target.style.backgroundColor = chosenColor;
+            canvas.style.borderColor = chosenColor;
+        }
+    } else if (colorOption == 'rainbow') {
+        if ((mouseDrag) && (e.target.className == 'pixel')) {
+            chosenColor = generateRandomColor();
             e.target.style.backgroundColor = chosenColor;
         }
     }
-});
+}
+)
 
-paintingColorList.addEventListener('click', (e) => {
-    let randomColor = null;
-    colors.querySelectorAll('div').forEach((item) => {
-        if (e.target.className == item.className) {
-            chosenColor = e.target.className;
-            canvas.style.borderColor = chosenColor;
-        }
+//shading options
+shading.addEventListener('click', (e) => {
 
-        if (e.target.className == 'subOptionRandomColor') {
-            randomColor = 'rgb(' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ')';
-            chosenColor = randomColor;
-            canvas.style.borderColor = randomColor;
-            console.log(`${randomColor}+'random color`);
-            console.log(`${chosenColor}+'random color`);
 
-        } else if (e.target.className == 'subOptionRainbow') {
-            canvas.addEventListener('mousemove', (e) => {
-                if (mouseDrag) {
-                    if (e.target.className == 'pixel') {
-                        rainbowColor = 'rgb(' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ',' + ((Math.random() * (255 - 0 + 1)) + 0) + ')';
-                        e.target.style.backgroundColor = rainbowColor;
-                        canvas.style.borderColor = rainbowColor;
-                        console.log(randomColor);
-                        // uncomment for true epilepsy mode
-                        // document.querySelector('body').style.backgroundColor = randomColor;
-                    }
-                }
-            }
-            )
+    if (e.target.className == 'subOptionDarker') {
+        shadingOption = 'darker';
+        shading.querySelector('.subOptionDarker').style.textDecoration = 'underline';
+        shading.querySelector('.subOptionLighter').style.textDecoration = 'none';
 
-        } else {
-            canvas.addEventListener('mousemove', (e) => {
-                if (mouseDrag) {
-                    if (e.target.className == 'pixel') {
-                        e.target.style.backgroundColor = chosenColor;
-                        randomColor = chosenColor;
-                        canvas.style.borderColor = chosenColor;
-                    }
-                }
-            }
-            )
-        }
-    })
+    } else if (e.target.className == 'subOptionLighter') {
+        shadingOption = 'lighter';
+        shading.querySelector('.subOptionLighter').style.textDecoration = 'underline';
+        shading.querySelector('.subOptionDarker').style.textDecoration = 'none';
+    }
 })
 
+//deletion options
+deleting.addEventListener('click', (e) => {
+    shading.querySelectorAll('li').forEach((item) => {
+        item.style.textDecoration = 'none';
+    });
+    if (e.target.classList.contains('subOptionEraser')) {
+
+        e.target.classList.toggle('underline');
+        erasing = true;
+        if ((e.target.classList.contains('underline')) || (erasing === undefined)) {
+            erasing = true;
+        } else erasing = false;
+
+    } else if (e.target.className == 'subOptionClear') {
+        document.querySelectorAll('.pixel').forEach((item) => {
+            item.removeAttribute('style', 'backgroundColor');
+            shadingOption = null;
+        })
+
+
+    }
+})
 
 //change canvas size according to slider and create canvasSize property
 range.addEventListener('input', (e) => {
@@ -82,9 +142,8 @@ range.addEventListener('input', (e) => {
     canvasSize = e.target.value;
 })
 
-
-//generate Canvas with set mesh density
-range.addEventListener('click', (e) => {
+//generate canvas with set mesh density
+range.addEventListener('click', () => {
     while (canvas.hasChildNodes()) {
         canvas.removeChild(canvas.lastChild);
     };
@@ -94,14 +153,14 @@ range.addEventListener('click', (e) => {
         pixel.classList.add('pixel');
         canvas.appendChild(pixel);
     }
+    colorLightness = getColorLightness(chosenColor);
 })
 
-//display or hide subOptions after click on optionList item
+//display or hide menus
 optionsList.forEach((item) => {
     item.addEventListener('click', (e) => {
 
         if ((e.target.classList.contains('icon')) || (e.target.classList.contains('text')) || (e.target.classList.contains('arrow'))) {
-            console.log('klikol si na option');
             if (e.target.parentElement.querySelector('.subOptions').classList.contains('sub-hidden')) {
                 e.target.parentElement.querySelector('.subOptions').classList.replace('sub-hidden', 'sub-displayed');
                 e.target.parentElement.querySelector('.arrow').classList.add('bx-rotate-180');
@@ -116,7 +175,7 @@ optionsList.forEach((item) => {
     })
 });
 
-//hide subOptions after mouse moves away
+//hide menus after mouse moves away
 document.addEventListener('mouseover', (e) => {
     if ((e.target.id == 'content') || (e.target.id == 'canvas') || (e.target.className == 'pixel') || (e.target.className == 'optionsList') || (e.target.className == 'canvasInitialMessage')) {
         document.querySelectorAll('.sub-displayed').forEach((item) => {
@@ -131,21 +190,24 @@ document.querySelector('.bx-pencil').addEventListener('click', (e) => {
     e.target.classList.toggle('bx-spin');
 });
 
-//mouseover painting
-// canvas.addEventListener('mouseover', (e) => {
-//     if (e.target.className == 'pixel') {
-//         e.target.style.backgroundColor = 'black';
-//     }
-// })
+function generateRandomColor() {
+    randomColorValue1 = (((Math.random() * (360 - 0 + 1)) + 0).toFixed(0));
+    randomColorValue2 = (((Math.random() * (100 - 0 + 1)) + 0).toFixed(0));
+    randomColorValue3 = (((Math.random() * (100 - 0 + 1)) + 0).toFixed(0));
+    return 'hsl(' + randomColorValue1 + ', ' + randomColorValue2 + '%, ' + randomColorValue3 + '%)';
+}
 
-//click and drag painting
-let mouseDrag = false;
-canvas.addEventListener('mousedown', () => {
-    mouseDrag = true;
-})
-canvas.addEventListener('mouseup', () => {
-    mouseDrag = false;
-})
+
+function getColorLightness(chosenColor) {
+    let hslColorCode = chosenColor.slice(4).split(',');
+    let colorValues = hslColorCode.map((value) => parseInt(value));
+    chosenColorValue1 = colorValues[0];
+    chosenColorValue2 = colorValues[1];
+    chosenColorValue3 = colorValues[2];
+    return Number(chosenColorValue3);
+}
+
+
 
 
 
